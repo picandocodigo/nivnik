22b43d517e81d65b1523e6c6ceeda8b31afb179f
Fernando Briano <fernando@picandocodigo.net>
2015-04-17 19:12:38 -0300

    Functional views for CRUD

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,31 @@
 class ContactsController < ApplicationController
 
   def index
-    
+    @page_title = "Contacts"
+    @contacts = Contact.all
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+    redirect_to @contact
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :surname, :description,
+                                    :phone_number, :email, :notes)
   end
 end
