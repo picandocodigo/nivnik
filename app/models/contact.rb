211639adae8b928fcab258c05ade066326b9cdb8
Fernando Briano <fernando@picandocodigo.net>
2015-04-17 21:22:27 -0300

    Changes for markup

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,12 @@ class Contact < ActiveRecord::Base
   def full_name
     !self.surname.empty? ? "#{self.name} #{self.surname}" : self.name
   end
+
+  def name_and_description
+    markup = <<-EOF
+    #{self.full_name}
+    <span>#{self.description}</span>
+    EOF
+    markup.html_safe
+  end
 end
