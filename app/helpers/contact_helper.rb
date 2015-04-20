d2f7dbfb1fecdacbd8060082c79d7108a3448b91
Fernando Briano <fernando@picandocodigo.net>
2015-04-20 16:54:37 -0300

    Refactor to add span to surname

--- app/helpers/contact_helper.rb
+++ app/helpers/contact_helper.rb
@@ -1,2 +1,10 @@
 module ContactHelper
+
+  def name_and_description(contact)
+    markup = []
+    markup << contact.name
+    markup << "<span>#{contact.surname}</span>" unless contact.surname.empty?
+    markup << "<span>#{contact.description}</span>"
+    markup.join("").html_safe
+  end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,15 +1,3 @@
 class Contact < ActiveRecord::Base
   validates :name, presence: true
-
-  def full_name
-    !self.surname.empty? ? "#{self.name} #{self.surname}" : self.name
-  end
-
-  def name_and_description
-    markup = <<-EOF
-    #{self.full_name}
-    <span>#{self.description}</span>
-    EOF
-    markup.html_safe
-  end
 end
