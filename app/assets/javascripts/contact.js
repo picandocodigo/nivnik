ab4c5075066e741efc63cb235470079c542c39d7
Fernando Briano <fernando@picandocodigo.net>
2015-04-20 16:34:24 -0300

    Change form submit with link on navigation

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded', function() {
+  var submit_button = document.getElementById('submit_form');
+  submit_button.addEventListener('click', function(){
+    document.getElementById('contacts').submit();
+  });
+});
